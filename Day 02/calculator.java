ba20a58077545701e4227140a8ab1eae4b5e141b
Divya160707 <24490.cs@rmkcet.ac.in>
2026-02-08 17:28:30 +0530

    Add files via upload

---
+++ Day 02/calculator.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first number:");
+        int a = sc.nextInt();
+        System.out.println("Enter second number:");
+        int b = sc.nextInt();
+        System.out.println("Enter operator:");
+        char op = sc.next().charAt(0);
+
+        switch (op) {
+            case '+':
+               System.out.println(a + b);
+               break;
+            case '-': 
+               System.out.println(a - b); 
+               break;
+            case '*': 
+               System.out.println(a * b); 
+               break;
+            case '/': 
+                System.out.println(a / b); 
+                break;
+            default:
+                System.out.println("Invalid operator");
+        }
+    }
+}
---
+++ Day 02/largestoftwo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class largestoftwo {
+public static void main(String[] args){
+   Scanner sc = new Scanner(System.in);
+   System.out.println("Enter a number:");
+   int num1 = sc.nextInt();
+   System.out.println("Enter a number:");
+   int num2 = sc.nextInt();
+   if(num1>num2){
+    System.out.println(num1 + " is the largest number.");
+   }
+   else{
+    System.out.println(num2 + " is the largest number.");
+  }
+}
+}
